fd44f87fb271336ee8130c98b1bf60dc3a9b1440
Henrique Melo <hacmelo@gmail.com>
2021-12-03 18:50:23 -0300

    paginas

---
+++ pages/conta.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col>
+      Conta
+    </v-col>
+  </v-row>
+</template>
---
+++ pages/sobre.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col>
+      Sobre
+    </v-col>
+  </v-row>
+</template>
