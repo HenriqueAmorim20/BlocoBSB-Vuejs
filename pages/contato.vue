ee73e52cbdd91d596ae481d4cb787d95f9b643b1
Henrique Melo <hacmelo@gmail.com>
2021-12-06 10:58:24 -0300

    Tela de contato

--- pages/contato.vue
+++ pages/contato.vue
@@ -1,7 +1,149 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col>
-      contato
+  <v-row>
+    <v-col align="center">
+      <v-card class="card">
+        <v-col align="start">
+          <span class="title">Mande uma mensagem!</span>
+        </v-col>
+        <v-col>
+          <v-form @submit.prevent="enviarMensagem()">
+            <v-row class="linha">
+              <v-col align="start">
+                <span>Email *</span>
+                <v-text-field
+                  v-model="formulario.email"
+                  clearable
+                  filled
+                  dense
+                  background-color="#00000022"
+                  color="white"
+                  placeholder="joseribeiro@gmail.com"
+                  required
+                  class="formfield"
+                />
+              </v-col>
+              <v-col align="start">
+                <span>Nome</span>
+                <v-text-field
+                  v-model="formulario.nome"
+                  clearable
+                  filled
+                  dense
+                  background-color="#00000022"
+                  color="white"
+                  placeholder="José Ribeiro da Silva"
+                  class="formfield"
+                />
+              </v-col>
+            </v-row>
+            <v-row class="linha">
+              <v-col align="start">
+                <span>Assunto</span>
+                <v-text-field
+                  v-model="formulario.assunto"
+                  clearable
+                  filled
+                  dense
+                  background-color="#00000022"
+                  color="white"
+                  placeholder="Compra de camisetas"
+                  class="formfield"
+                />
+              </v-col>
+            </v-row>
+            <v-row class="linha">
+              <v-col align="start">
+                <span>Mensagem *</span>
+                <v-textarea
+                  v-model="formulario.mensagem"
+                  filled
+                  background-color="#00000022"
+                  color="white"
+                  placeholder="Olá, tudo bem? gostaria de saber se é possível comprar várias camisetas de uma vez."
+                  clearable
+                  required
+                  class="formfield"
+                />
+              </v-col>
+            </v-row>
+            <v-row class="linha">
+              <v-col align="end">
+                <v-btn class="btn" dark type="submit">Enviar</v-btn>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-col>
+      </v-card>
     </v-col>
   </v-row>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formulario: {
+        email: null,
+        nome: null,
+        assunto: null,
+        mensagem: null,
+      }
+    }
+  },
+  methods: {
+    enviarMensagem () {
+      if (!this.validateForm()) return this.$alert.info('Campos obrigatórios incorretos!')
+      try {
+        this.$alert.success('Obrigado pela sua mensagem!')
+      } catch (error) {
+        this.$alert.error('Não foi possível enviar sua mensagem!')
+      }
+    },
+
+    validateForm() {
+      return this.formulario.mensagem && this.formulario.email &&(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.formulario.email)) && this.formulario.mensagem.length > 3
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  max-width: 700px;
+  background: rgba(255, 255, 255, 0.705);
+  margin: 1rem 0;
+}
+
+.title {
+  text-align: center;
+  font-size: 1.3rem;
+  font-weight: 300;
+  border-left: 9px solid #000;
+  color: #000;
+  padding: 0 12px;
+  margin: 0 0.5rem;
+}
+
+.linha {
+  color: black;
+}
+
+.linha span {
+  font-weight: 300;
+  color: #000;
+}
+
+.formfield {
+  min-width: 220px;
+  margin-top: 6px;
+}
+
+.btn {
+  width: 200px;
+  box-shadow: inset 0px 0px 0px #2F3B47;
+  transition: all 0.5s !important;
+}
+.btn:hover {
+  box-shadow: inset 250px 0px 0px #518fb8;
+}
+</style>
